07b4fc64e15f18d06206a617302b10f510f4eb41
GhostStead <ghoststead@ghoststead.com>
2020-07-11 08:51:51 -0700

    Initial commit using ghost-theme-bootstrap as a base.

---
+++ README.md
@@ -0,0 +1,41 @@
+# GhostStead Ghost theme
+
+A business Ghost theme built with [Bootstrap v4](https://getbootstrap.com/), the most popular HTML5 framework.
+
+**Goals**:
+* Pass gscan
+* Integrate Bootstrap with minimal impact to site performance.
+* Use SASS for CSS.
+
+## Prerequisites
+* NodeJS (v12.13.x)
+* git (optional)
+
+## Setup
+Either clone this repo or download the tarball/zip file, then install the package
+dependencies.
+
+```shell script
+npm install
+```
+
+## Build
+After setup is completed (once), the build commands are essentially the same as
+the default Casper theme.
+
+```shell script
+npm run zip
+```
+
+There is also a development mode that supports livereload.
+```shell script
+npm run dev
+```
+
+## Installation
+After `zip` completes successfully, install the resulting bundle from the
+`dist` directory using the Ghost admin interface.
+
+***
+
+This repository is maintained by [GhostStead](https://www.ghoststead.com).
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const pump = require('pump');
+
+// gulp plugins and utils
+const livereload = require('gulp-livereload');
+const zip = require('gulp-zip');
+var sass = require('gulp-sass');
+
+function serve(done) {
+    livereload.listen();
+    done();
+}
+
+function hbs(done) {
+    pump([
+        gulp.src(['*.hbs', 'partials/**/*.hbs']),
+        livereload()
+    ], done);
+}
+
+function scss(done) {
+    pump([
+        gulp.src('scss/*.scss', {sourcemaps: true}),
+        sass(),
+        gulp.dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], done);
+}
+
+function js(done) {
+    pump([
+        gulp.src('assets/js/*.js', {sourcemaps: true}),
+        gulp.dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], done);
+}
+
+function zipper(done) {
+    const pkg = require('./package.json');
+    const targetDir = 'dist/';
+    const filename = pkg.name + '-' + pkg.version + '.zip';
+
+    pump([
+        gulp.src([
+            '**',
+            '!node_modules', '!node_modules/**',
+            '!dist', '!dist/**'
+        ]),
+        zip(filename),
+        gulp.dest(targetDir)
+    ], done);
+}
+
+const scssWatcher = () => gulp.watch('assets/scss/**', scss);
+const hbsWatcher = () => gulp.watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+const watcher = gulp.parallel(scssWatcher, hbsWatcher);
+const build = gulp.series(scss, js);
+const dev = gulp.series(build, serve, watcher);
+
+exports.build = build;
+exports.zip = gulp.series(build, zipper);
+exports.default = dev;
