710aa96c2cccb21c6eb06d0f2bc89ae87c2e2fd7
GhostStead <ghoststead@ghoststead.com>
2021-01-01 10:15:35 -0700

    Separate particlesJS using a webpack export

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: require.resolve('particles.js'),
+                use:
+                    'exports-loader?type=commonjs&exports[]=particlesJS',
+            },
+        ],
+    }
 };
