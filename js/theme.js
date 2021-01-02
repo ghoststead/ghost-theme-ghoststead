bc946bbce91168560961b3523fdf83686dfa9ecd
GhostStead <ghoststead@ghoststead.com>
2021-01-02 14:20:49 -0700

    Modularize pie-chart

--- js/theme.js
+++ js/theme.js
@@ -1,49 +1,5 @@
-/* Start Pie Chart */
-function customPieChart(){
-    var chart = document.getElementsByClassName('pie_chart_in');
-    if(chart.length > 0) {
-        for (var i = 0; i < chart.length; i++) {
-            customSetPieCharts(chart[i]);
-        }
-    }
-}
-function customSetPieCharts(p){
-    var pie_chart_size = p.getAttribute('data-size') || '160',
-        pie_chart_animate = p.getAttribute('data-animate') || '2000',
-        pie_chart_width = p.getAttribute('data-width') || '6',
-        pie_chart_color = p.getAttribute('data-color') || '#84ba3f',
-        pie_chart_track_color = p.getAttribute('data-trackcolor') || 'rgba(0,0,0,0.10)',
-        pie_chart_line_Cap = p.getAttribute('data-lineCap') || 'round';
-    var childNodes = p.querySelector('span.middle');
-
-    new EasyPieChart(p, {
-        size: Number(pie_chart_size),
-        animate: Number(pie_chart_animate),
-        trackColor: pie_chart_track_color,
-        lineWidth: Number(pie_chart_width),
-        barColor: pie_chart_color,
-        scaleColor: false,
-        lineCap: pie_chart_line_Cap,
-        onStep: function (from, to, percent) {
-            childNodes.innerHTML = Math.round(percent)+'%';
-        }
-    });
-}
-/* End Pie Chart */
-/* END particlesJS */
 (function() {
-    var checkPieChartLoaded = false;
-    window.addEventListener('scroll', function() {
-        var element = document.getElementsByClassName('piechartcontainer');
-        if (element.length > 0) {
-            var position = element[0].getBoundingClientRect();
-            // checking for partial visibility
-            if (position.top < window.innerHeight && position.bottom >= 0 && !checkPieChartLoaded) {
-                checkPieChartLoaded = true;
-                customPieChart();
-            }
-        }
-    });
+
     /*
      * Enable forms that have a data-form-type attribute set to 'contact' by:
      * - append a hidden form element containing the customer api key (if not present).
